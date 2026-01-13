a6016e4c81535a3480f654b7e0c353a48d6985fb
subahan islam <subahanislam523@gmail.com>
2026-01-13 11:55:39 +0600

    added googleAuthetication complete and routes controller

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,7 +1,9 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { AuthController } from "./auth.controller";
 import { Role } from "../user/user.interface";
 import { checkAuth } from "../../middlewares/checkAuth";
+import passport from "passport";
+import { envVars } from "../../config/env";
 
 const router = Router();
 
@@ -12,6 +14,26 @@ router.post(
   "/reset-password",
   checkAuth(...Object.values(Role)),
   AuthController.resetPassword
+);
+
+router.get(
+  "/google",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const redirect = req.query.redirect || "/";
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+      state: redirect as string,
+    })(req, res, next);
+  }
+);
+
+// api/v1/auth/google/callback?state=/booking
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: `${envVars.FRONT_END_URL}/login?error=There is some issues with your account. Please contact with out support team!`,
+  }),
+  AuthController.googleCallbackController
 );
 
 export const AuthRoutes = router;
