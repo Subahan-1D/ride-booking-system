316b5facdd3e4ff5ae35f9d13d4fd9b6d7ffbfb0
subahan islam <subahanislam523@gmail.com>
2026-01-19 20:16:15 +0600

    added to error handler or cast error duplicate error validation error zod error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,19 +1,76 @@
+// globalErrorHandler.ts
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import AppError from "../errorHelpers/AppError";
-export const globalErrorHandler = (
+import { TErrorSources } from "../interfaces/error.types";
+import { handleDuplicateError } from "../helpers/handleDuplicateError";
+import { handleCastError } from "../helpers/handleCastError";
+import { handleZodError } from "../helpers/handleZodError";
+import { handleValidationError } from "../helpers/handleValidationError";
+
+// import AppError from "../errorHelpers/AppError";
+
+export const globalErrorHandler = async (
   err: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  if (envVars.NODE_ENV === "development") {
+    console.log(err);
+  }
+
+  // console.log({ file: req.files });
+
+  //   if (req.file) {
+  //     await deleteImageFromCloudinary(req.file.path);
+  //   }
+
+  //   if (req.files && Array.isArray(req.files) && req.files.length) {
+  //     const imageUrls = (req.files as Express.Multer.File[]).map(
+  //       (file) => file.path
+  //     );
+
+  //     await Promise.all(imageUrls.map((url) => deleteImageFromCloudinary(url)));
+  //   }
+
+  let errorSources: TErrorSources[] = [];
   let statusCode = 500;
+  let message = "Something Went Wrong!!";
 
-  let message = "Something went wrong ";
+  // Duplicate error
+  if (err.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
 
-  if (err instanceof AppError) {
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  }
+
+  // CastError
+  else if (err.name === "CastError") {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  }
+
+  // ZodError
+  else if (err.name === "ZodError") {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources as TErrorSources[];
+  }
+
+  // ValidationError
+  else if (err.name === "ValidationError") {
+    const simplifiedError = handleValidationError(err);
+
+    statusCode = simplifiedError.statusCode;
+    errorSources = simplifiedError.errorSources as TErrorSources[];
+    message = simplifiedError.message;
+  } else if (err instanceof AppError) {
     statusCode = err.statusCode;
     message = err.message;
   } else if (err instanceof Error) {
@@ -24,7 +81,8 @@ export const globalErrorHandler = (
   res.status(statusCode).json({
     success: false,
     message,
-    error: err.message,
+    errorSources,
+    err: envVars.NODE_ENV === "development" ? err : null,
     stack: envVars.NODE_ENV === "development" ? err.stack : null,
   });
 };
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -5,37 +5,10 @@ import { envVars } from "../../config/env";
 import AppError from "../../errorHelpers/AppError";
 import {
   createNewAccessTokenWithRefreshToken,
-  createUserTokens,
 } from "../../utils/userTokens";
-import {  IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import bcryptjs from "bcryptjs";
 import httpStatus from "http-status-codes";
-const credentialsLogin = async (payload: Partial<IUser>) => {
-  const { email, password } = payload;
-
-  const isUserExists = await User.findOne({ email });
-  if (!isUserExists) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist");
-  }
-
-  const isPasswordMatched = await bcryptjs.compare(
-    password as string,
-    isUserExists.password as string
-  );
-  if (!isPasswordMatched) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Password is incorrect");
-  }
-
-  const userTokens = createUserTokens(isUserExists);
-  const { password: pass, ...rest } = isUserExists.toObject();
-
-  return {
-    accessToken: userTokens?.accessToken,
-    refreshToken: userTokens?.refreshToken,
-    user: rest,
-  };
-};
 const getNewAccessToken = async (refreshToken: string) => {
   const newAccessToken = await createNewAccessTokenWithRefreshToken(
     refreshToken
@@ -68,7 +41,7 @@ const resetPassword = async (
 };
 
 export const AuthServices = {
-  credentialsLogin,
+
   getNewAccessToken,
   resetPassword,
 };
