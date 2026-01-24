402f83faa06b09c9f489ba549865e5c349b0fc12
subahan islam <subahanislam523@gmail.com>
2026-01-24 19:58:59 +0600

    added to by a driver all and ride all

--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -1,29 +1,26 @@
 import { Response } from "express";
 
 interface TMeta {
-  // page: number;
-  // limit: number;
-  // totalPage: number;
+  page: number;
+  limit: number;
+  totalPage: number;
   total: number;
 }
 
 interface TResponse<T> {
   statusCode: number;
-  message: string;
   success: boolean;
+  message: string;
   data: T;
-  meta ?: TMeta;
+  meta?: TMeta;
 }
 
-const sendResponse = <T>(res: Response, data: TResponse<T>) => {
+export const sendResponse = <T>(res: Response, data: TResponse<T>) => {
   res.status(data.statusCode).json({
     statusCode: data.statusCode,
-    message: data.message,
     success: data.success,
+    message: data.message,
     meta: data.meta,
     data: data.data,
   });
 };
-
-
-export default sendResponse;
